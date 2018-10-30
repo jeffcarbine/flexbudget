fded8a6c7138d7110127b5759a326e36ec4ceb0c
Jeffrey Carbine <jcarbine@dhicorp.com>
2018-10-30 14:27:23 -0600

    changed db to flexbudget

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 // config/database.js
 module.exports = {
 
-    'url' : 'mongodb://localhost/ourmusichub' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'url' : 'mongodb://localhost/flexbudget' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 
 };

8960026ed87df9e88f0099f4ce146727c3b2dd2c
Jeffrey Carbine <jcarbine@dhicorp.com>
2018-10-30 15:25:08 -0600

    variable values

--- app/models/variables.js
+++ app/models/variables.js
@@ -2,8 +2,9 @@ var mongoose = require('mongoose');
 
 // define the schema for our user model
 var variablesSchema = mongoose.Schema({
-    yearly  : Object,
-    monthly : Object
+    yearBudget  : Object,
+    monthBudget : Object,
+    flex        : Number
 });
 
 // create the model for users and expose it to our app
